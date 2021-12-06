8da1ebd00f9135864fe011bfd5b8de579ea49a5a
Tilak Patidar <patidartilak120@gmail.com>
2021-12-06 09:54:09 +0530

    prod.js modified

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,3 @@ module.exports={
     database : process.env.MONGOURI,
     secret :process.env.JWT_SEC
 }
-
-// "mongodb+srv://InstagramClone:Te8PDHpWIS1K3Cfn@cluster0.fibw7.mongodb.net/InstagramClone?retryWrites=true&w=majority"
